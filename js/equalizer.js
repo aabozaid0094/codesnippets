46b7fb585d75122d40e52346e8f4aa41d0dde15b
Ahmed Abo Zaid <56685532+aabozaid0094@users.noreply.github.com>
2021-02-14 13:00:18 +0200

    Update equalizer.js

--- js/equalizer.js
+++ js/equalizer.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
   $(window).on("load", function() {
     $('.equalized-group').each(function(){
       let heighest = 0;
-      $('.equalized-item').each(function(){
+      $(this).find('.equalized-item').each(function(){
         heighest = ($(this).height() > heighest) ? $(this).height() : heighest;
       });
       $('.equalized-item').height(heighest)
